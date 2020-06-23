dca6773330646b78f7068994802a370cd91bbe51
Andre <sgavsnake@gmail.com>
2020-06-22 21:10:00 -0300

    ...

--- src/main/java/com/desafio/codenation/config/ProdConfig.java
+++ src/main/java/com/desafio/codenation/config/ProdConfig.java
@@ -1,13 +1,16 @@
 package com.desafio.codenation.config;
 
+import com.desafio.codenation.services.DBService;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
+import java.text.ParseException;
 
 @Configuration
 @Profile("prod")
@@ -16,10 +19,16 @@ public class ProdConfig {
     @Value("${spring.datasource.url}")
     private String dbUrl;
 
+    @Autowired
+    private DBService dbService;
+
     @Bean
-    public DataSource dataSource() {
+    public DataSource dataSource() throws ParseException {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(dbUrl);
+
+        dbService.instantiateTestDatabase();
+
         return new HikariDataSource(config);
     }
 }
