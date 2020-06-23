83d10d5a32df733e57819a09d3090ee10dc237af
Andre <sgavsnake@gmail.com>
2020-06-23 00:45:38 -0300

    app properties for test and pro envs

--- src/main/java/com/desafio/codenation/config/PostgresqlDatabaseConfig.java
+++ src/main/java/com/desafio/codenation/config/PostgresqlDatabaseConfig.java
@@ -1,7 +1,10 @@
 package com.desafio.codenation.config;
 
+import com.desafio.codenation.services.DBService;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import io.swagger.annotations.Authorization;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,16 +13,22 @@ import javax.sql.DataSource;
 import java.text.ParseException;
 
 @Configuration
-
 public class PostgresqlDatabaseConfig {
+
+
 
     @Value("${spring.datasource.url}")
     private String dbUrl;
+
+    @Autowired
+    private DBService dbService;
 
     @Bean
     public DataSource dataSource() throws ParseException {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(dbUrl);
+
+        dbService.instantiateTestDatabase();
         return new HikariDataSource(config);
     }
 }
